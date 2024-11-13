79caabe16284378bbacf8fecfea2ba60c62df6c5
BarnabasNovak1 <barnabas.novak01@gmail.com>
2024-11-12 16:36:58 -0800

    added files

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,17 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
+import { Avatar } from "./Avatar";
 
 export const Experience = () => {
+  const texture = useTexture("textures/background.jpg");
+  const viewport = useThree((state) => state.viewport);
+
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Avatar position={[0, -3, 5]} scale={2} />
+      <Environment preset="sunset" />
+ 
     </>
   );
 };
