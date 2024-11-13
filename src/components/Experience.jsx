092ce802831377b7b99a44b6301e45a4cb5083bf
BarnabasNovak1 <barnabas.novak01@gmail.com>
2024-11-12 16:48:19 -0800

    first commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,7 +3,7 @@ import { useThree } from "@react-three/fiber";
 import { Avatar } from "./Avatar";
 
 export const Experience = () => {
-  const texture = useTexture("textures/background.jpg");
+  const texture = useTexture("textures/youtubeBackground.jpg");
   const viewport = useThree((state) => state.viewport);
 
   return (
@@ -11,7 +11,10 @@ export const Experience = () => {
       <OrbitControls />
       <Avatar position={[0, -3, 5]} scale={2} />
       <Environment preset="sunset" />
- 
+      <mesh>
+        <planeGeometry args={[viewport.width, viewport.height]} />
+        <meshBasicMaterial map={texture} />
+      </mesh>
     </>
   );
 };
